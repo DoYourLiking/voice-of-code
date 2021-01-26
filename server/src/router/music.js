1e7ca637fe719baf6327d69a18642f465114c116
yuzhanglong <yuzl1123@163.com>
2021-01-26 22:41:36 +0800

    fix(typo): 修改变量名的错误

--- server/src/router/music.js
+++ server/src/router/music.js
@@ -3,13 +3,13 @@ const { musicGenerate } = require('../middleware/musicGenerate')
 const { codeAnalysis } = require('../middleware/codeAnalysis')
 const { codeUpload } = require('../middleware/codeUpload')
 
-const fileRouter = new Router({ prefix: '/music' })
+const musicRouter = new Router({ prefix: '/music' })
 
 // 上传方式分析代码 && 生成音乐
-fileRouter.post('/get_by_upload', codeUpload, codeAnalysis, musicGenerate)
+musicRouter.post('/get_by_upload', codeUpload, codeAnalysis, musicGenerate)
 
 // 直接 post 代码 && 生成音乐
-fileRouter.post('/get_by_code', codeAnalysis, musicGenerate)
+musicRouter.post('/get_by_code', codeAnalysis, musicGenerate)
 
 
-module.exports = fileRouter
+module.exports = musicRouter
