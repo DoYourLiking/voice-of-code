38ec3f3e285941d67c0a3a996ba19b047cb825c0
DoYourLiking <linus_zk@163.com>
2021-01-27 23:07:05 +0800

    配置跨域

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,14 @@ const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
 const useRoutes = require('./router')
 const errorHandler = require('./core/errorHandler')
+const cors = require('koa-cors');
+
 
 const app = new Koa()
 
 app.useRoutes = useRoutes
-
+//设置跨域
+app.use(cors());
 // 处理异常
 app.use(errorHandler)
 
